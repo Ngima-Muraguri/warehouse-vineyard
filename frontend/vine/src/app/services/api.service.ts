8be91c3c5c4312ea972befb40f5b3541f6eadb36
C0159412020 <adlightakinyi1@gmail.com>
2022-07-12 12:00:25 +0300

    components

--- frontend/vine/src/app/services/api.service.ts
+++ frontend/vine/src/app/services/api.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+getProduct () {
+  let url = 'http://127.0.0.1:8000/api/product';
+  
+  return this.http.get(url);
 }
+
+
+getPrice (){
+  let url = 'http://127.0.0.1:8000/api/price'
+  return this.http.get(url);
+}
+
+  
+  }
+
+
+
